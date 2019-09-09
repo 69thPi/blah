f4ac1a7a982eb8f2eff428bead3d55a8158432dc
meDaddy_ <tanmaysaraiya@gmail.com>
2019-09-09 22:40:52 +0400

    update rocketEngine.py

--- my work/Rocket_Engine/nozzle_design&perfm/rocketEngine.py
+++ my work/Rocket_Engine/nozzle_design&perfm/rocketEngine.py
@@ -1,6 +1,6 @@
-pc=2067000   #combustion chamber pressure
-tc=2861      #combustion chamber max. temp
-mol_mass=28  #mol mass of products
+pc=2067000   #combustion chamber pressure Pa
+tc=2861      #combustion chamber max. temp K
+mol_mass=28  #mol mass of products gms
 gamma=1.23
 alt=401      #in kms
-thrust=1300  #newton
+thrust=1300  #Newton
